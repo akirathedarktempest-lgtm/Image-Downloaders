f90dfbb3020d5869bdf4c0a7900c0fb38f860324
Yuzui <akirathedarktempest@gmail.com>
2026-09-15 12:03:15 +0530

    Implement FastAPI for image retrieval from SQLite

---
+++ raw/Test Number 3 (API).py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI
+import sqlite3
+
+app=FastAPI()
+
+@app.get("/images/<type>/<ids>")
+def getImages(types:str,ids:int):
+    if types=="pfps" or types=="wallpapers" or types=="fandoms":
+        if ids>number(types):
+            return {"Out of the range":":("}
+        connect=sqlite3.connect("Image.db")
+        cursor=connect.cursor()
+        cursor.execute(f"""CREATE TABLE IF NOT EXISTS {types}(
+                            id INT,
+                            image BLOB,
+                            credit TEXT,
+                            type TEXT)""")
+        cursor.execute(f"SELECT * FROM {types} WHERE ids={ids}")
+        data=cursor.fetchone()
+        if ids==number(types):
+            check="yes"
+        else:
+            check="no"
+        connect.close()
+        return {"image":data[1].decode("utf-8"),"credit":data[2],"last?":check}
+    else:
+        return {"no type found":":("}
+
+def number(types:str):
+    if types=="pfps" or types=="wallpapers" or types=="fandoms":
+        connect=sqlite3.connect("Image.db")
+        cursor=connect.cursor()
+        cursor.execute(f"""CREATE TABLE IF NOT EXISTS {types}(
+                        id INT,
+                        image BLOB,
+                        credit TEXT,
+                        type TEXT)""")
+        cursor.execute(f"SELECT * FROM {types}")
+        info=cursor.fetchall()
+        connect.close()
+        return len(info)
+    else:
+        return 0
